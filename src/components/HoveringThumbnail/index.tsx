dbf701ab58a75fcf49a7ae7fb88761184ce77084
fecapark <fecapark@gmail.com>
2025-06-20 03:40:22 +0900

    :sparkles: 썸네일에 동적 위아래 변경 기능 추가

--- src/components/HoveringThumbnail/index.tsx
+++ src/components/HoveringThumbnail/index.tsx
@@ -1,14 +1,27 @@
 import clsx from 'clsx'
-import { HTMLMotionProps, motion, useMotionValue, useSpring } from 'motion/react'
-import { RefObject, useRef, useState } from 'react'
+import { HTMLMotionProps, motion, useMotionValue, useSpring, Variants } from 'motion/react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 
 import { config } from '@/components/config'
 import { useHoverThumbnailInfo } from '@/components/Providers/HoverThumbnailInfoProvider'
+import { useStageSize } from '@/hooks/useStageSize'
 
 type HoveringThumbnailProps = HTMLMotionProps<'div'> & {
   ref?: RefObject<HTMLDivElement | null>
   src?: string
   thumbnailType: 'image' | 'video'
+}
+
+const exitVariants: Variants = {
+  initial: {
+    opacity: 0,
+    visibility: 'hidden',
+    transition: { duration: 0.1 },
+  },
+  animate: {
+    opacity: 1,
+    transition: { duration: 0.2 },
+  },
 }
 
 export const HoveringThumbnail = ({
@@ -24,11 +37,32 @@ export const HoveringThumbnail = ({
 }: React.PropsWithChildren<HoveringThumbnailProps>) => {
   const motionRef = useRef<HTMLDivElement>(null)
   const { onHover } = useHoverThumbnailInfo()
+  const { stageHeight } = useStageSize()
   const [isHovering, setIsHovering] = useState(false)
+  const [mediaHeight, setMediaHeight] = useState(0)
+
   const x = useSpring(useMotionValue(0), { stiffness: 300, damping: 30 })
   const y = useSpring(useMotionValue(0), { stiffness: 300, damping: 30 })
 
   const realRef = ref || motionRef
+
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (!videoRef.current) {
+      return
+    }
+    if (thumbnailType !== 'video') {
+      return
+    }
+
+    if (isHovering) {
+      videoRef.current.currentTime = 0
+      videoRef.current.play()
+    } else {
+      videoRef.current.pause()
+    }
+  }, [isHovering, thumbnailType])
 
   if (config.printMode) {
     return children
@@ -46,8 +80,13 @@ export const HoveringThumbnail = ({
           return
         }
         const { top, left } = realRef.current.getBoundingClientRect()
+
+        const yVal = e.clientY - top
+        const bottom = e.clientY + mediaHeight
+        const isReversed = bottom > stageHeight
+
         x.jump(e.clientX - left)
-        y.jump(e.clientY - top)
+        y.jump(isReversed ? yVal - mediaHeight : yVal)
         setIsHovering(true)
         onHover()
         onHoverStart?.(e, i)
@@ -57,28 +96,56 @@ export const HoveringThumbnail = ({
           return
         }
         const { top, left } = realRef.current.getBoundingClientRect()
+
+        const yVal = e.clientY - top
+        const bottom = e.clientY + mediaHeight
+        const isReversed = bottom > stageHeight
+
         x.set(e.clientX - left)
-        y.set(e.clientY - top)
+        y.set(isReversed ? yVal - mediaHeight : yVal)
         onPointerMove?.(e)
       }}
       ref={realRef}
       {...props}
     >
       {children}
-      {isHovering && src && (
+      {src && (
         <motion.div
           className="pointer-events-none absolute top-2 left-2 z-50 select-none"
           style={{ x, y }}
         >
-          {thumbnailType === 'image' ? (
-            <div className="shadow-dialog max-w-[35vw] overflow-hidden rounded-xl">
-              <img className="object-contain" src={src} />
-            </div>
-          ) : (
-            <div className="shadow-dialog h-fit w-[35vw] overflow-hidden rounded-xl">
-              <video autoPlay className="object-contain" loop muted src={src} />
-            </div>
-          )}
+          <motion.div
+            animate={isHovering ? 'animate' : 'initial'}
+            style={{ willChange: 'opacity' }}
+            variants={exitVariants}
+          >
+            {thumbnailType === 'image' ? (
+              <div className="shadow-dialog max-w-[35vw] overflow-hidden rounded-xl">
+                <img
+                  className="object-contain"
+                  onLoad={(e) => {
+                    const { height } = e.currentTarget.getBoundingClientRect()
+                    setMediaHeight(height)
+                  }}
+                  src={src}
+                />
+              </div>
+            ) : (
+              <div className="shadow-dialog h-fit w-[35vw] overflow-hidden rounded-xl">
+                <video
+                  className="object-contain"
+                  loop
+                  muted
+                  onCanPlay={(e) => {
+                    const { height } = e.currentTarget.getBoundingClientRect()
+                    setMediaHeight(height)
+                  }}
+                  ref={videoRef}
+                  src={src}
+                />
+              </div>
+            )}
+          </motion.div>
         </motion.div>
       )}
     </motion.div>
